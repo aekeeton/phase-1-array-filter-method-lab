34ec0f337bd4fdf2d2c7013f79d0f0237c3b7409
aekeeton <ashley.keeton@outlook.com>
2022-06-08 15:24:26 -0500

    commit tree d0059b51ecff32917a45308916ec8a8c863e86c5

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+const { arrayContaining } = require("expect");
+
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Anette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, string) {
+    let answer = drivers.filter(el => el.toLowerCase() === string.toLowerCase());
+    return answer;
+}  
+
+function fuzzyMatch(array, string) {
+    return array.filter(function(el) {
+        return el.startsWith(string)
+    })
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(function(hometown)
+    {
+        return hometown.name === name
+    })
+}
